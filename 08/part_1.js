020ee17e07fb890cd4eb0155544df92eac7bb72a
Michal Kozubik <kozubik.michal@gmail.com>
2021-12-21 23:36:24 +0100

    Day 08: Part 1

---
+++ 08/part_1.js
@@ -0,0 +1,40 @@
+import input from "./input.js";
+
+/**
+	0:      1:      2:      3:      4:
+ aaaa    ....    aaaa    aaaa    ....
+b    c  .    c  .    c  .    c  b    c
+b    c  .    c  .    c  .    c  b    c
+ ....    ....    dddd    dddd    dddd
+e    f  .    f  e    .  .    f  .    f
+e    f  .    f  e    .  .    f  .    f
+ gggg    ....    gggg    gggg    ....
+
+	5:      6:      7:      8:      9:
+ aaaa    aaaa    aaaa    aaaa    aaaa
+b    .  b    .  .    c  b    c  b    c
+b    .  b    .  .    c  b    c  b    c
+ dddd    dddd    ....    dddd    dddd
+.    f  e    f  .    f  e    f  .    f
+.    f  e    f  .    f  e    f  .    f
+ gggg    gggg    ....    gggg    gggg
+ */
+
+let sum = 0;
+
+input.forEach((line) => {
+	const [patterns, output] = line.split("|").map((part) => part.trim().split(" "));
+
+	output.forEach((signal) => {
+		switch (signal.length) {
+			case 2:
+			case 3:
+			case 4:
+			case 7:
+				sum++;
+				break
+		}
+	})
+})
+
+console.log(sum);
